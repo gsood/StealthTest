22fe1fc9c5427292f8c612be6d9a020a80449b53
Gaurav Sood <gsood@marklogic.com>
2017-04-20 11:15:20 +0100

    Second commit

--- src/test/scala/CafeMenuSpec.scala
+++ src/test/scala/CafeMenuSpec.scala
@@ -4,16 +4,17 @@ class CafeMenuSpec extends WordSpecLike with Matchers {
 
   "Cafe Menu" should {
     "list Cola correctly" in {
-      ???
+      CafeMenu.items should contain ("Cola" -> MenuItem("Cola", COLD, 50))
     }
     "list Coffee correctly" in {
-      ???
+      CafeMenu.items should contain ("Coffee" -> MenuItem("Coffee", HOT, 100))
     }
     "list Cheese Sandwich correctly" in {
-      ???
+      CafeMenu.items should contain ("Cheese Sandwich" -> MenuItem("Cheese Sandwich", COLD, 200))
+
     }
     "list Steak Sandwich correctly" in {
-      ???
+      CafeMenu.items should contain ("Steak Sandwich" -> MenuItem("Steak Sandwich", HOT, 450))
     }
   }
 
