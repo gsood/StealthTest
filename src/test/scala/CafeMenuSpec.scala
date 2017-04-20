fcba59e50f8e3e083947e95a96044da1bf1c8fd2
Gaurav Sood <gsood@marklogic.com>
2017-04-20 13:11:42 +0100

    Sixth commit

--- src/test/scala/CafeMenuSpec.scala
+++ src/test/scala/CafeMenuSpec.scala
@@ -4,33 +4,44 @@ class CafeMenuSpec extends WordSpecLike with Matchers {
 
   "Cafe Menu" should {
     "list Cola correctly" in {
-      CafeMenu.items should contain ("Cola" -> MenuItem("Cola", COLD, 50))
+      CafeMenu.items should contain ("Cola" -> Drink("Cola", COLD, 50))
     }
     "list Coffee correctly" in {
-      CafeMenu.items should contain ("Coffee" -> MenuItem("Coffee", HOT, 100))
+      CafeMenu.items should contain ("Coffee" -> Drink("Coffee", HOT, 100))
     }
     "list Cheese Sandwich correctly" in {
-      CafeMenu.items should contain ("Cheese Sandwich" -> MenuItem("Cheese Sandwich", COLD, 200))
+      CafeMenu.items should contain ("Cheese Sandwich" -> Food("Cheese Sandwich", COLD, 200))
 
     }
     "list Steak Sandwich correctly" in {
-      CafeMenu.items should contain ("Steak Sandwich" -> MenuItem("Steak Sandwich", HOT, 450))
+      CafeMenu.items should contain ("Steak Sandwich" -> Food("Steak Sandwich", HOT, 450))
     }
   }
 
   "Cafe Menu billing" should {
     "generate correct bill amount for two coffees and one Cola" in {
-      CafeMenu.billAmount(List("Coffee", "Coffee", "Cola")) should be ("2.5")
+      CafeMenu.totalBillAmt(List("Coffee", "Coffee", "Cola")) should be ("2.50")
     }
     "generate correct bill amount for one coffees,  one Cola and one Cheese Sandwich" in {
-      CafeMenu.billAmount(List("Coffee", "Cheese Sandwich", "Cola")) should be ("3.5")
+      CafeMenu.totalBillAmt(List("Coffee", "Cheese Sandwich", "Cola")) should be ("3.85")
     }
   }
 
   "Cafe Menu" should {
     "Apply no service tax for drinks only order" in {
-      CafeMenu.serviceCharge(List("Coffee", "Cola")) should be(0)
+      CafeMenu.totalBillAmt(List("Coffee", "Coffee", "Cola", "Cola")) should be ("3.00")
     }
-    
+
+    "Apply 10% service tax for cold food  order" in {
+      CafeMenu.totalBillAmt(List("Coffee", "Cola", "Cheese Sandwich")) should be("3.85")
+    }
+
+    "Apply 20% service tax for hot food  order" in {
+      CafeMenu.totalBillAmt(List("Coffee", "Cola", "Steak Sandwich")) should be("7.20")
+    }
+
+    "Apply 20% service tax for hot food order but limit service charge to £20" in {
+      CafeMenu.totalBillAmt((1 to 100).map(_ => "Steak Sandwich").toList) should be("470.00")
+    }
   }
 }
