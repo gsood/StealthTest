0fb214befc447ddb61c73d48ab6bfb3e5582b70e
Gaurav Sood <gsood@marklogic.com>
2017-04-20 10:46:30 +0100

    First commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "Stealth"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+resolvers += "Artima Maven Repository" at "http://repo.artima.com/releases"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+libraryDependencies +="junit" % "junit" % "4.11" % "test"
---
+++ src/test/scala/CafeMenuSpec.scala
@@ -0,0 +1,20 @@
+import org.scalatest.{Matchers, WordSpecLike}
+
+class CafeMenuSpec extends WordSpecLike with Matchers {
+
+  "Cafe Menu" should {
+    "list Cola correctly" in {
+      ???
+    }
+    "list Coffee correctly" in {
+      ???
+    }
+    "list Cheese Sandwich correctly" in {
+      ???
+    }
+    "list Steak Sandwich correctly" in {
+      ???
+    }
+  }
+
+}
